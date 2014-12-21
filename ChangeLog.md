6ce35f5df5d280793c2b8baebbb8f37b0cc0ba25
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2014-12-21 22:41:57 +0100

    update build

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,12 +1,12 @@
 Vision ChangeLog
 ================
 
-# Version 0.1.0 (IN PROGRESS)
+# Version 0.1.0 (22/12/2014)
 
+- Add cAdvisor 0.7.0 for monitoring
 - Update build script for release
-- Configure ElasticSearch
-- Configure Kibana
-- Configure Graphana
-- Configure InfluxDB
-- Configure Fluentd
+- Configure ElasticSearch 1.4.0
+- Configure Kibana 4.0.0-beta2
+- Configure Graphana 1.9.0
+- Configure InfluxDB 0.8.7
 - Init project
--- addons/release.sh
+++ addons/release.sh
@@ -44,7 +44,6 @@ download_fig_darwin() {
     chmod +x $APP-$VERSION-darwin/fig
 }
 
-
 release() {
     OS=$1
     VERSION=$2
@@ -63,11 +62,13 @@ release() {
     GRAFANA_VERSION=$(image_version "grafana")
     INFLUXDB_VERSION=$(image_version "influxdb")
     FLUENTD_VERSION=$(image_version "fluentd")
+    CADVISOR_VERSION="0.7.0"
     sed -i "s/ES_VERSION/$ES_VERSION/g" $APP-$VERSION-$OS/fig.yml
     sed -i "s/KIBANA_VERSION/$KIBANA_VERSION/g" $APP-$VERSION-$OS/fig.yml
     sed -i "s/GRAFANA_VERSION/$GRAFANA_VERSION/g" $APP-$VERSION-$OS/fig.yml
     sed -i "s/INFLUXDB_VERSION/$INFLUXDB_VERSION/g" $APP-$VERSION-$OS/fig.yml
     sed -i "s/FLUENTD_VERSION/$FLUENTD_VERSION/g" $APP-$VERSION-$OS/fig.yml
+    sed -i "s/CADVISOR_VERSION/$FLUENTD_VERSION/g" $APP-$VERSION-$OS/fig.yml
     tar cf - $APP-$VERSION-$OS | gzip > $APP-$VERSION-$OS.tar.gz
     rm -fr $APP-$VERSION-$OS
 }
